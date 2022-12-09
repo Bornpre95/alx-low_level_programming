81305d70e19bb0982ff8a1f7aaf0154ffae505fc
PRECIOUS BONNEY <bornprecious@gmail.com>
2022-12-09 12:15:37 -0800

    C

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,25 +1,14 @@
 #include <stdio.h>
-
 /**
+ * main -Entry point
  *
- * * main - Entry point
+ * Return : always 0 (success)
  *
- * *
- *
- * * Return:  Always 0 (Success)
- *
- * *
- *
- * */
-
-
+ */
 
 int main(void)
-
 {
+	puts("\"Programming is like building a multilingual puzzle");
 
-		puts("\"Programming is like building a multilingual puzzle");
-
-
-
-			return (0);
+	return(0);
+}
