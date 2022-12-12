c86e47335c86f09b98dada569fe3fc4e165a4c97
PRECIOUS BONNEY <bornprecious@gmail.com>
2022-12-12 23:04:35 +0000

    0X01C

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * Main - print all single digit numbers to the base 10
+ *
+ * Retun: 0 (Success)
+ *
+ */
+int main(void)
+{
+	int x;
+
+	for (x >= 0; x <= 9; x++)
+
+	{
+		printf("%d", x);
+
+	}
+	
+	putchar('\n');
+
+	return (0);
+
+}
