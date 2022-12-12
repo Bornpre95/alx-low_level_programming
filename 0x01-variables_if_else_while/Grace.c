75a8fa25f3e476624a3bd720ad61d53e545f102e
PRECIOUS BONNEY <bornprecious@gmail.com>
2022-12-12 21:30:01 +0000

    0x01C

---
+++ 0x01-variables_if_else_while/Grace.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+*main - Entry point of the program
+*
+* return: always 0 (Success)
+*
+*/
+int main(void)
+
+{
+	int x = 10;
+
+	if (x > 5)
+
+		printf("Grace is beautiful\n");
+
+return (0);
+
+}
