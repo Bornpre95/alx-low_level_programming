09116fbaf43fe99e929b6266d5b3a8f1b8bdb824
PRECIOUS BONNEY <bornprecious@gmail.com>
2022-12-15 00:22:10 +0000

    loops

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_alphabet -- print the aphabets with the function void print alphabet
+ *
+ * return: always 0 (success)
+ *
+ */
+void print_alphabet(void)
+{
+	int i;
+
+	for (i = 'a'; i <= 'z'; i++)
+
+	{
+		_putchar(i);
+
+	}
+
+	_putchar('\n');
+
+}
