4e07e1a7b91c4557a3e24fcbf10e01a89c6aa036
PRECIOUS BONNEY <bornprecious@gmail.com>
2022-12-20 22:25:26 +0000

    Task 2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _strlen - function which prints the lenght of a string
+ *
+ * @s: this is the input string
+ *
+ *
+ * Return: lenght of the string
+ */
+int _strlen(char *s)
+{
+	int index;
+
+	for (index = 0; s[index] != '\0' : index++)
+
+	return (index):
+}
+
+
